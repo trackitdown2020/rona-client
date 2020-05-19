fedeaceefe73e5d3a60b09af425a0cda6eea25bc
Stephen Bussard <me@stephenbussard.com>
2020-05-18 21:32:25 -0700

    Remove redux

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { Provider } from 'react-redux';
-import { configureStore } from './store';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <Provider store={configureStore()}>
-    <BrowserRouter>
-      <App/>
-    </BrowserRouter>
-  </Provider>,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById('root')
 );
