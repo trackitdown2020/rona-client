f1b0c52c076e4f725dd295d54fe3dc2ebfe941f5
Jeff Chen <jeffreychen006@gmail.com>
2020-06-09 21:11:38 -0700

    Added basic animation

--- src/components/StatsDisplayCard/index.jsx
+++ src/components/StatsDisplayCard/index.jsx
@@ -4,6 +4,7 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import { useCountUp } from 'use-count-up';
 
 const useStyles = makeStyles({
     root: {
@@ -20,6 +21,12 @@ const useStyles = makeStyles({
 
 function StatsDisplayCard({title, value}) {
     const classes = useStyles();
+    const { value:countValue } = useCountUp({
+        isCounting: true,
+        shouldUseToLocaleString: true,
+        end: value,
+        duration: 2
+    }); 
     return (
         <Card className={classes.root}>
             <CardContent className={classes.content}>
@@ -28,7 +35,7 @@ function StatsDisplayCard({title, value}) {
                 </Typography>
                 <Divider />
                 <Typography variant="h4" color="textSecondary" className={classes.values}>
-                    { value.toLocaleString() }
+                    { countValue }
                 </Typography>
             </CardContent>
         </Card>
