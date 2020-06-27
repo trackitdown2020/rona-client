6e5915a856220efe217c1819853ab4663b4b7601
Jeff Chen <jeffreychen006@gmail.com>
2020-06-26 23:34:43 -0700

    Added settings icon

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -6,30 +6,44 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { Breadcrumbs } from '../Breadcrumbs';
 import { makeStyles } from '@material-ui/core/styles';
+import SettingsIcon from '@material-ui/icons/Settings';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
 const drawerWidth = 450;
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
+    flexGrow: 1
   },
   appBar: {
-    zIndex: theme.zIndex.drawer + 1,
+    zIndex: theme.zIndex.drawer + 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1
+  },
+  toolbar: {
+    display: 'flex',
+    justifyContent: 'space-between'
   },
   drawer: {
     width: drawerWidth,
-    flexShrink: 0,
+    flexShrink: 0
   },
   drawerPaper: {
-    width: drawerWidth,
+    width: drawerWidth
   },
   drawerContainer: {
-    overflow: 'auto',
+    overflow: 'auto'
   },
   main: {
-    flexGrow: 1,
+    flexGrow: 1
   },
-  content: {},
+  content: {}
 }));
 
 export function Layout({ title, children, renderToolbar }) {
@@ -39,24 +53,28 @@ export function Layout({ title, children, renderToolbar }) {
     <div className={classes.root}>
       <CssBaseline />
       <AppBar position="fixed" className={classes.appBar}>
-        <Toolbar>
-          <Typography variant="h6" noWrap>
+        <Toolbar classes={classes.toolbar}>
+          {/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton> */}
+          <Typography variant="h6" className={classes.title}>
             {title}
           </Typography>
+          <Button color="inherit">
+            <SettingsIcon />
+          </Button>
         </Toolbar>
       </AppBar>
       <main className={classes.main}>
         <Toolbar />
-        <div className={classes.content}>
-          { children }
-        </div>
-        <Breadcrumbs/>
+        <div className={classes.content}>{children}</div>
+        <Breadcrumbs />
       </main>
       <Drawer
         className={classes.drawer}
         variant="permanent"
         classes={{
-          paper: classes.drawerPaper,
+          paper: classes.drawerPaper
         }}
         anchor="right"
       >
--- src/state/AppStateProvider.js
+++ src/state/AppStateProvider.js
@@ -4,10 +4,22 @@ import { AppContext } from './AppContext';
 
 export const AppStateProvider = ({ children }) => {
   const [selectedCountry, setSelectedCountry] = useState({});
+  const [openSettingsModal, setOpenSettingsModal] = useState(false);
+
+  const handleOpenSettingsModal = () => {
+    setOpenSettingsModal(true);
+  }
+
+  const handleCloseSettingsModal = () => {
+    setOpenSettingsModal(false);
+  }
 
   const appState = {
     selectedCountry,
     setSelectedCountry,
+    openSettingsModal,
+    handleOpenSettingsModal,
+    handleCloseSettingsModal
   };
 
   return <AppContext.Provider value={appState} children={children} />;
