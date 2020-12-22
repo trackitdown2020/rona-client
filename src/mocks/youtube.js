c7be832280394ec4f0533fff8d0fa8ea1848aeb2
djleng <dleng@usc.edu>
2020-12-22 15:14:04 -0800

    update

---
+++ src/mocks/youtube.js
@@ -0,0 +1,112 @@
+export const mockYoutubeData = [
+    {
+        "id": {},
+        "snippet": {
+            "publishedAt": "2020-12-18T13:20:43Z",
+            "title": "Vice President receives COVID-19 vaccine on live TV l ABC News",
+            "description": "U.S. Surgeon General Dr. Jerome Adams and the vice president's wife also received the vaccination to encourage Americans to get vaccinated. #ABCNews ...",
+            "channelTitle": "ABC News"
+        },
+        "kind": "youtube#searchResult",
+        "etag": "uqZVoSp1ZE9C0ANv2RHXH5uzFJ4"
+    },
+    {
+        "id": {},
+        "snippet": {
+            "publishedAt": "2020-08-15T12:29:28Z",
+            "title": "Coronavirus update: The latest COVID-19 news from Asia | DW News",
+            "description": "00:00 US On a visit to Taiwan, Health Secretary Alex Azar has accused China of \"resisting information sharing\" and failing to \"warn the world\" on the COVID 19 ...",
+            "channelTitle": "DW News"
+        },
+        "kind": "youtube#searchResult",
+        "etag": "8fDdQFSbHMlVOPT4VwdMSZ_zTTI"
+    },
+    {
+        "id": {},
+        "snippet": {
+            "publishedAt": "2020-12-15T14:44:07Z",
+            "title": "Fauci on what to know about COVID-19 vaccine l GMA",
+            "description": "Dr. Anthony Fauci shares the latest facts on the vaccine, including when we will know how long immunity lasts. LEARN MORE: Coronavirus live updates: Fauci ...",
+            "channelTitle": "Good Morning America"
+        },
+        "kind": "youtube#searchResult",
+        "etag": "V4rBFlpJ7UcediglKV2rEo4ma-k"
+    },
+    {
+        "id": {},
+        "snippet": {
+            "publishedAt": "2020-12-07T20:16:24Z",
+            "title": "ABC News Live Update: US reports record 1.3 million new COVID-19 cases last week",
+            "description": "Plus, 129 Americans are testing positive for COVID-19 every minute in the U.S. and all eyes are on the Georgia Senate runoff races.",
+            "channelTitle": "ABC News"
+        },
+        "kind": "youtube#searchResult",
+        "etag": "xCqabthIUiiG8FTj1i_CqjcuEHc"
+    },
+    {
+        "id": {},
+        "snippet": {
+            "publishedAt": "2020-07-26T13:45:57Z",
+            "title": "Coronavirus update: The latest COVID-19 news from around the world | DW News",
+            "description": "00:00 In the United States, record numbers of jobless workers are facing uncertainty as they wait for Congress to decide on whether they'll extend a boost to ...",
+            "channelTitle": "DW News"
+        },
+        "kind": "youtube#searchResult",
+        "etag": "BYTT0pB_7SAO4AFXXcF6EknmRj0"
+    },
+    {
+        "id": {},
+        "snippet": {
+            "publishedAt": "2020-10-12T18:48:53Z",
+            "title": "The current Covid-19 virus is not the same one we saw in March: Holy Name Medical Center CEO",
+            "description": "Michael Maron, Holy Name Medical Center CEO, joins 'Power Lunch' to discuss the rise of Covid-19 cases at his hospital, the symptoms patients are exhibiting ...",
+            "channelTitle": "CNBC Television"
+        },
+        "kind": "youtube#searchResult",
+        "etag": "_UIJLAdAzmVT9wt7FxQM9qEnqaA"
+    },
+    {
+        "id": {},
+        "snippet": {
+            "publishedAt": "2020-10-26T23:51:59Z",
+            "title": "Some Good News About a Possible COVID-19 Vaccine",
+            "description": "We could be one step closer to a coronavirus vaccine. The Oxford COVID-19 vaccine has prompted immune response in some young adults and elderly people.",
+            "channelTitle": "extratv"
+        },
+        "kind": "youtube#searchResult",
+        "etag": "J1t0NCOKlTfG9aulsnwihHE2orM"
+    },
+    {
+        "id": {},
+        "snippet": {
+            "publishedAt": "2020-12-11T17:00:10Z",
+            "title": "COVID-19 Surge Hits South Korea And Japan After They Had Contained The Virus | TODAY",
+            "description": "The U.S. is far from alone in the fight against COVID-19: Parts of Asia, where the virus first hit and which were the first to emerge from lockdowns, are now ...",
+            "channelTitle": "TODAY"
+        },
+        "kind": "youtube#searchResult",
+        "etag": "EULdnf4ylBmuOxBFMp-GL-nx0wA"
+    },
+    {
+        "id": {},
+        "snippet": {
+            "publishedAt": "2020-12-09T02:09:49Z",
+            "title": "US marks 15,000 COVID-19 deaths in a week",
+            "description": "Los Angeles County deputies arrested 158 people after a massive house party. The sheriff said California's tighter restrictions will lead to more underground ...",
+            "channelTitle": "ABC News"
+        },
+        "kind": "youtube#searchResult",
+        "etag": "OV61SlLxkp2J1odpvABvU8inoBY"
+    },
+    {
+        "id": {},
+        "snippet": {
+            "publishedAt": "2020-07-17T12:30:44Z",
+            "title": "Coronavirus: India&#39;s Covid-19 cases surge past one million - BBC News",
+            "description": "India added a record number of Covid-19 cases - nearly 35000 - in the last 24 hours, breaching the one million mark. It has the world's third-largest case load, ...",
+            "channelTitle": "BBC News"
+        },
+        "kind": "youtube#searchResult",
+        "etag": "PVl4-W6o7M2WMc5OC7aykbZFKhY"
+    }
+]
